ebb251a500b9cb16b41df30f7f95afe6c79fe304
leomet07 <leomet07@gmail.com>
2024-04-08 20:49:45 -0400

    lint

--- quickstart.py
+++ quickstart.py
@@ -1,69 +1,72 @@
-import  os.path
+import os.path
 
-from  google.auth.transport.requests  import  Request
-from  google.oauth2.credentials  import  Credentials
-from  google_auth_oauthlib.flow  import  InstalledAppFlow
-from  googleapiclient.discovery  import  build
-from  googleapiclient.errors  import  HttpError
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from pprint import pprint
 
 #  If  modifying  these  scopes,  delete  the  file  token.json.
-SCOPES  =  ["https://www.googleapis.com/auth/drive.metadata.readonly", "https://www.googleapis.com/auth/drive"]
+SCOPES = [
+    "https://www.googleapis.com/auth/drive.metadata.readonly",
+    "https://www.googleapis.com/auth/drive",
+]
 
-def  get_creds():
-	creds  =  None
-	#  The  file  token.json  stores  the  user's  access  and  refresh  tokens,  and  is
-	#  created  automatically  when  the  authorization  flow  completes  for  the  first
-	#  time.
-	if  os.path.exists("token.json"):
-		creds  =  Credentials.from_authorized_user_file("token.json",  SCOPES)
-	#  If  there  are  no  (valid)  credentials  available,  let  the  user  log  in.
-	if  not  creds  or  not  creds.valid:
-		if  creds  and  creds.expired  and  creds.refresh_token:
-			creds.refresh(Request())
-		else:
-			flow  =  InstalledAppFlow.from_client_secrets_file(
-					"credentials.json",  SCOPES
-			)
-			creds  =  flow.run_local_server(port=0)
-		#  Save  the  credentials  for  the  next  run
-		with  open("token.json",  "w")  as  token:
-			token.write(creds.to_json())
-	return creds
+
+def get_creds():
+    creds = None
+    #  The  file  token.json  stores  the  user's  access  and  refresh  tokens,  and  is
+    #  created  automatically  when  the  authorization  flow  completes  for  the  first
+    #  time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    #  If  there  are  no  (valid)  credentials  available,  let  the  user  log  in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        #  Save  the  credentials  for  the  next  run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return creds
+
 
 def get_service(creds):
-	service  =  build("drive",  "v3",  credentials=creds)
-	return service
-
-def  main():
-	"""Shows  basic  usage  of  the  Drive  v3  API.
-	Prints  the  names  and  ids  of  the  first  10  files  the  user  has  access  to.
-	"""
-	creds = get_creds()
+    service = build("drive", "v3", credentials=creds)
+    return service
 
 
-	try:
-		service  = get_service(creds)
+def main():
+    """Shows  basic  usage  of  the  Drive  v3  API.
+    Prints  the  names  and  ids  of  the  first  10  files  the  user  has  access  to.
+    """
+    creds = get_creds()
 
-		#  Call  the  Drive  v3  API
-		results  =  (
-				service.files()
-				.list(pageSize=10,  fields="nextPageToken,  files(id,  name)")
-				.execute()
-		)
-		items  =  results.get("files",  [])
+    try:
+        service = get_service(creds)
 
-		if  not  items:
-			print("No  files  found.")
-			return
-		print("Files:")
-		for  item  in  items:
-			pprint(item)
-			print(f"{item['name']}  ({item['id']})")
-	except  HttpError  as  error:
-		#  TODO(developer)  -  Handle  errors  from  drive  API.
-		print(f"An  error  occurred:  {error}")
+        #  Call  the  Drive  v3  API
+        results = (
+            service.files()
+            .list(pageSize=10, fields="nextPageToken,  files(id,  name)")
+            .execute()
+        )
+        items = results.get("files", [])
+
+        if not items:
+            print("No  files  found.")
+            return
+        print("Files:")
+        for item in items:
+            pprint(item)
+            print(f"{item['name']}  ({item['id']})")
+    except HttpError as error:
+        #  TODO(developer)  -  Handle  errors  from  drive  API.
+        print(f"An  error  occurred:  {error}")
 
 
-if  __name__  ==  "__main__":
-	main()
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -2,20 +2,22 @@ from quickstart import get_creds, get_service
 from pprint import pprint
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
 creds = get_creds()
 
 
-service  = get_service(creds)
+service = get_service(creds)
 
-file_id  = os.getenv("TEST_FILE_ID")
+file_id = os.getenv("TEST_FILE_ID")
 
 
-file = service.files().get(fileId=file_id).execute()
+comments = service.comments().list(fileId=file_id, fields="comments").execute()
 
-print(file)
-comments  =  service.comments().list(fileId=file_id,fields='comments').execute()            
-for  comment  in  comments.get('comments'):                  
-	pprint(comment)
+all_comments = list(comments.get("comments"))
+# for  comment  in  comments.get('comments'):
+# 	pprint(comment)
+
+print(json.dumps(all_comments, indent=4))
